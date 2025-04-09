db63297ad97fee5eec13cdf7c17cd5cffb9a99af
Andrew Marshall <andrew.marshall@slalom.com>
2025-04-09 14:28:33 -0600

    Add terraform fmt check

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,25 +11,25 @@ resource "aws_ecr_repository" "lambda_repo" {
 }
 
 resource "aws_db_instance" "aurora" {
-  identifier           = "demo-aurora"
+  identifier          = "demo-aurora"
   engine              = "aurora-mysql"
   instance_class      = "db.t3.medium"
   username            = "admin"
-  password            = "insecure123"  # Intentionally insecure
+  password            = "insecure123" # Intentionally insecure
   skip_final_snapshot = true
   # Intentionally not encrypting storage
-  storage_encrypted   = false
+  storage_encrypted = false
 }
 
 resource "aws_lambda_function" "hello_world" {
-  filename         = "../src/lambda_function.zip"
-  function_name    = "hello-world"
-  role            = aws_iam_role.lambda_role.arn
-  handler         = "lambda_function.handler"
-  runtime         = "python3.9"
+  filename      = "../src/lambda_function.zip"
+  function_name = "hello-world"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.handler"
+  runtime       = "python3.9"
 
-  package_type    = "Image"
-  image_uri       = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
+  package_type = "Image"
+  image_uri    = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -53,16 +53,16 @@ resource "aws_lb" "front_end" {
   name               = "demo-lb"
   internal           = false
   load_balancer_type = "application"
-  
+
   # Intentionally not using HTTPS
-  enable_http2       = true
-  
-  subnets = ["subnet-12345678", "subnet-87654321"]  # Example subnet IDs
+  enable_http2 = true
+
+  subnets = ["subnet-12345678", "subnet-87654321"] # Example subnet IDs
 }
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.front_end.arn
-  port              = "80"  # Intentionally using HTTP
+  port              = "80" # Intentionally using HTTP
   protocol          = "HTTP"
 
   default_action {
